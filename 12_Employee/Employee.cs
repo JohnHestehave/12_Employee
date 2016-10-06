8d43f39baf6cafc8a615264ed4bf81c43934ef07
John <John@JOHN>
2016-10-06 09:27:14 +0200

    Add project files.

---
+++ 12_Employee/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12_Employee
+{
+	public class Employee
+	{
+		public int Id;
+		public string Name;
+		public string Type;
+
+		public Employee(string name, string type, int id)
+		{
+			Id = id;
+			Name = name;
+			Type = type;
+		}
+	}
+}
---
+++ 12_Employee/EmployeeRepository.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12_Employee
+{
+	public class EmployeeRepository
+	{
+		List<Employee> Employees;
+
+		public EmployeeRepository()
+		{
+			Employees = new List<Employee>();
+		}
+
+		public Employee CreateEmployee(string name, string job)
+		{
+			Employee e = new Employee(name, job, CountEmployees()+1);
+			Employees.Add(e);
+			return e;
+		}
+
+		public void SaveEmployee(Employee e)
+		{
+			
+		}
+
+		public Employee LoadEmployee(int id)
+		{
+			Employee e = Employees[id-1];
+			return e;
+		}
+
+		public void Clear()
+		{
+			Employees.Clear();
+		}
+
+		public int CountEmployees()
+		{
+			return Employees.Count();
+		}
+
+		public List<Employee> FindAllEmployees()
+		{
+			return Employees;
+		}
+
+	}
+}
